d9be616b9ac28337bc9c4a60cb0f9bc82c09085f
SnorlaxH <snorlaxh.me@gmail.com>
2026-06-01 21:24:35 +0900

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -2,18 +2,6 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(req: NextRequest) {
-    const token = req.cookies.get('accessToken')?.value
-
-    const isLoginPage = req.nextUrl.pathname === '/login'
-
-    if (!token && !isLoginPage) {
-        return NextResponse.redirect(new URL('/login', req.url))
-    }
-
-    if (token && isLoginPage) {
-        return NextResponse.redirect(new URL('/', req.url))
-    }
-
     return NextResponse.next()
 }
 
